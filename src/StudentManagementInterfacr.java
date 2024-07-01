fd510b9b51d1804a17dc7374f658940a0d0fe02e
Maurice12370 <maurice12370@gmail.com>
2024-06-30 20:29:59 -0400

    Framework for Library Check in/out Sys - 05/19

---
+++ src/StudentManagementInterfacr.java
@@ -0,0 +1,8 @@
+public interface StudentManagementInterfacr  {
+
+    viod addStudent();
+    void updateStudent();
+    void deleteStudent();
+    void searchStudent();
+    void generateStudent();
+}
