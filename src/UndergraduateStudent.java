068fb5b061247f249321c3592b24aa473d714fc4
Maurice12370 <maurice12370@gmail.com>
2024-07-01 18:47:20 -0400

    Student Management System

---
+++ src/UndergraduateStudent.java
@@ -0,0 +1,21 @@
+public class UndergraduateStudent extends Student {
+    private int year;
+
+    public UndergraduateStudent(String name, int age, String studentID, String course, int year) {
+        super(name, age, studentID, course);
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public void updateInfo(int age) {
+        super.updateInfo(age);
+    }
+}
