410b95e7cfb4c15d34d7c05c34e97a0d13400cdb
tsingh50 <tsingh5@umbc.edu>
2024-07-03 18:30:39 -0400

    Update UndergraduateStudent.java

--- src/UndergraduateStudent.java
+++ src/UndergraduateStudent.java
@@ -18,4 +18,24 @@ public class UndergraduateStudent extends Student {
     public void updateInfo(int age) {
         super.updateInfo(age);
     }
+}public class UndergraduateStudent extends Student {
+    private int year;
+
+    public UndergraduateStudent(String name, int age, String studentID, String course, int year) {
+        super(name, age, studentID, course);
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public void updateInfo(int age) {
+        super.updateInfo(age);
+    }
 }
