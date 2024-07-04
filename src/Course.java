e60122c6ae81aef0f7c1d2eabe28d9ce4a889e83
Maurice12370 <114260713+Maurice12370@users.noreply.github.com>
2024-07-03 22:13:20 -0400

    Update Course.java

--- src/Course.java
+++ src/Course.java
@@ -1,31 +1,25 @@
-import java.util.ArrayList;
-import java.util.List;
 public class Course {
-    //fields
     private String courseName;
-    private String courseID;
-    private List<Student> enrolledStudents;
-    //overloaded constructor
-    public Course(String courseName, String courseID){
+    private String courseCode;
+
+    public Course(String courseName, String courseCode) {
         this.courseName = courseName;
-        this.courseID = courseID;
-        this.enrolledStudents = new ArrayList<>();
+        this.courseCode = courseCode;
     }
-    //getters
-    public String getCourseName(){
+
+    public String getCourseName() {
         return courseName;
     }
-    public String getCourseID(){
-        return courseID;
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
     }
-    public List<Student> getEnrolledStudents(){
-        return enrolledStudents;
+
+    public String getCourseCode() {
+        return courseCode;
     }
-    //add and remove students
-    public void addStudent(Student student){
-        enrolledStudents.add(student);
-    }
-    public void removeStudent(Student student){
-        enrolledStudents.remove(student);
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
     }
 }
